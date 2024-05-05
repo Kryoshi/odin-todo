a74677000cc6c81b33cdbae2779af64cd286a539
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-05-05 21:54:10 +0530

    Set up project

---
+++ webpack.common.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+
+  entry: {
+
+    app: './src/index.js',
+
+  },
+
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+
+    ],
+
+  },
+
+  plugins: [
+
+    new HtmlWebpackPlugin({
+
+      template: './src/index.html',
+
+    }),
+
+  ],
+
+  output: {
+
+    filename: '[name].bundle.js',
+
+    path: path.resolve(__dirname, 'dist'),
+
+    clean: true,
+
+  },
+
+};
